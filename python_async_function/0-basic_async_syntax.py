9da7be09c53cbc9fb77cde447d2f2fca58961dc2
freddyrosa16 <freddy.rosa16@gmail.com>
2024-04-24 10:40:29 -0400

    Add basic async syntax implementation in python_async_function

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    delay = random.randrange(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
