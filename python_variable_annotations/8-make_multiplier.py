8933b741f49ef83a0ba611d0197292b98d03d28b
freddyrosa16 <freddy.rosa16@gmail.com>
2024-04-24 10:20:03 -0400

    Refactor make_multiplier function to include a more descriptive docstring and improve variable naming in python_variable_annotations

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -5,7 +5,7 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """ This function takes a float as argument and returns a function that multiplies a float. """
-    def mult(num: float) -> float:
-        return num * multiplier
-
+    def mult(n: float) -> float:
+        """ This function takes a float and multiply. """
+        return n * multiplier
     return mult
