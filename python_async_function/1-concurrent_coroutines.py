c0f404a318b091c540f1b673f36f868af1cb98e8
freddyrosa16 <freddy.rosa16@gmail.com>
2024-04-24 11:26:45 -0400

    Add async function wait_n to python_async_function

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" This async function takes 2 int. Return the list of the delay.  """
+from typing import List
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    results = [await wait_random(max_delay) for _ in range(n)]
+    return results
