fb372b0b4eaabbb419250aaa5ff037425ce69ffe
freddyrosa16 <freddy.rosa16@gmail.com>
2024-04-24 11:33:53 -0400

    Fix bug in wait_n function by sorting the results in python_async_function

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,4 +8,4 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Store  the results in a list. """
     results = [await wait_random(max_delay) for _ in range(n)]
-    return results
+    return results.sort()
