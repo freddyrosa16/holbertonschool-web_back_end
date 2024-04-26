1c7fedfa9e049f86074c4ab8bd625f46726821bb
freddyrosa16 <freddy.rosa16@gmail.com>
2024-04-26 07:33:52 -0400

    Refactor wait_n function to use asyncio.as_completed in python_async_function

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -7,5 +7,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Store  the results in a list. """
-    results = [await wait_random(max_delay) for _ in range(n)]
-    return results.sort()
+    routines = [randwait(max_delay) for i in range(n)]
+    return [await x for x in asyncio.as_completed(routines)]
