869810c24569b38f3f64ef3afd3b68e2336fbc3f
freddyrosa16 <freddy.rosa16@gmail.com>
2024-04-26 07:52:29 -0400

    Refactor task_wait_random function to use asyncio.create_task instead of creating a Task object in python_async_function

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-""" The regular function syntax task 3. """
+"""the regular function syntax task 3"""
 import asyncio
-random = __import__('0-basic_async_syntax').wait_random
+
+randwait = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int):
-    """ Task wait random. """
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """task wait random"""
     return asyncio.create_task(randwait(max_delay))
