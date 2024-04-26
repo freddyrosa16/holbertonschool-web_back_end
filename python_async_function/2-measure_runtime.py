738cc881d693bae0c489d5459f84fc874e1bc07c
freddyrosa16 <freddy.rosa16@gmail.com>
2024-04-26 07:37:58 -0400

    Refactor shebang line in 9-element_length.py to use env python3 in python_variable_annotations

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-""" Measure runtime. """
+"""Measure runtime"""
 import asyncio
 import time
 
-wait = __import__('1-concurrent_coroutines.py').wait_n
+wait = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """ Measure runtime. """
+    """measure runtime"""
     start_time = time.perf_counter()
     asyncio.run(wait(n, max_delay))
     end_time = time.perf_counter()
