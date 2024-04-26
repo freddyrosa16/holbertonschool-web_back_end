d6248fbb8aa1142eee7392e562bdefce8a922759
freddyrosa16 <freddy.rosa16@gmail.com>
2024-04-26 07:51:58 -0400

    Refactor wait_random function to use random.uniform instead of random.randrange in python_async_function

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-""" This async function takes an int that waits for a random delay. """
-import random
+"""async syntax"""
+
 import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """ Async function that takes the time of the delay. """
-    delay = random.randrange(0, max_delay)
+    """wait random"""
+    delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
