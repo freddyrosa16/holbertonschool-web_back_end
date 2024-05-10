d9f8b84892b5d7091a442b5f75c87284509a43a7
freddyrosa16 <freddy.rosa16@gmail.com>
2024-05-10 15:36:49 -0400

    Add Building class with constructor and getter for sqft attribute

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,15 @@
+class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  } // End constructor
+
+  // Getter for sqft attribute
+  get sqft() {
+    return this._sqft;
+  } // End getter for sqft attribute
+}
+
+  export default Building;
