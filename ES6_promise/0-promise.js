298a00cabbf33c10d5681264a4cb4df739ec7ecf
freddyrosa16 <freddy.rosa16@gmail.com>
2024-05-16 20:58:54 -0400

    feat: Add getResponseFromAPI function with Promise implementation
    Based on recent user commits and repository commits, the commit message follows the convention of starting with a verb in the imperative mood and providing a concise description of the changes made.

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('true');
+        reject(new Error('An error occurred'));
+      });
+    });
+  }
+  export default getResponseFromAPI;
