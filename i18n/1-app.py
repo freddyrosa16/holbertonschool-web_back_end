3c3e2b5d67bdfbd4b03d627249d3f34b0651ccf0
freddyrosa16 <freddy.rosa16@gmail.com>
2024-10-27 22:49:27 -0400

    Refactor i18n app configuration and templates for improved accessibility, SEO, and user experience

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
 """
-Welcome to Holberton
+Welcome Holberton
 """
 from flask import Flask, render_template
 from flask_babel import Babel
+app = Flask(__name__)
+babel = Babel(app)
 
 
-class Config:
+class Config(object):
     """
-    Config class for setting available languages and default locale and timezone
+    languages config
     """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app = Flask(__name__)
 app.config.from_object(Config)
-
-babel = Babel(app)
 
 
 @app.route("/", methods=['GET'])
@@ -26,8 +25,4 @@ def helloWorld():
     """
     Hello world
     """
-    return render_template('0-index.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template('1-index.html')
