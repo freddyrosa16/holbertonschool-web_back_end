955111c9e919e8fb49780069b8d8485d3b20f4bf
freddyrosa16 <freddy.rosa16@gmail.com>
2024-10-27 22:41:58 -0400

    Refactor i18n app configuration and templates for improved accessibility, SEO, and user experience

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,42 +1,47 @@
 #!/usr/bin/env python3
-""" Route module for the API - Force locale with URL parameter """
-from flask import Flask, request, render_template
-from flask_babel import Babel
-from os import getenv
+"""
+Welcome to Holberton
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config:
+    """
+    Config class for setting available languages and default locale and timezone
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
 babel = Babel(app)
 
 
-class Config(object):
-    """ Setup - Babel configuration """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-app.config.from_object('4-app.Config')
-
-
-@app.route('/', methods=['GET'], strict_slashes=False)
-def index() -> str:
-    """ GET /
-    Return: 4-index.html
+def get_locale():
     """
-    return render_template('4-index.html')
+    Determine the best match with our supported languages.
+    Check for locale parameter in request args.
+    """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-def get_locale() -> str:
-    """ Determines best match for supported languages """
-    if request.args.get('locale'):
-        locale = request.args.get('locale')
-        if locale in app.config['LANGUAGES']:
-            return locale
-    else:
-        return request.accept_languages.best_match(app.config['LANGUAGES'])
+babel.init_app(app, locale_selector=get_locale)
 
 
-if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+@app.route("/", methods=['GET'])
+def helloWorld():
+    """
+    Hello world
+    """
+    return render_template('0-index.html', title=_("home_title"), header=_("home_header"))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
